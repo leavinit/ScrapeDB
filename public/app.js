9ca8a4e9dde95ee1d30a53b5ecee7ab5db6b0c99
Adam S <leavinit@gmail.com>
2019-06-18 22:23:23 -0500

    added fetch button and adjusted routes accordingly to update page on button press

--- public/app.js
+++ public/app.js
@@ -1,14 +1,24 @@
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link +
-      "<br /></br>" + data[i].summary + "</br></br></p>");
-    }
+  
+  $("#scrapeButton").click(function(e){
+      e.preventDefault();
+      $.ajax({
+          method: "GET",
+          url: "/scrape"
+      })
+      .then(function(){
+          // Grab the articles as a json
+        $.getJSON("/articles", function(data) {
+            // For each one
+            for (var i = 0; i < data.length; i++) {
+            // Display the apropos information on the page
+            $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link +
+            "<br /></br>" + data[i].summary + "</br></br></p>");
+            }
+        });
+
+      });
+
   });
-  
-  
   // Whenever someone clicks a p tag
   $(document).on("click", "p", function() {
     // Empty the notes from the note section
